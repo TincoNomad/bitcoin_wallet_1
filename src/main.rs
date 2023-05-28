6e24ef5bf1901350b0a137adb0f7ab4a39a312d2
Renzo Tincopa <rtincopa@gmail.com>
2023-05-28 18:32:58 -0500

    en la primera versión se veia el ambiente, ya lo retiramos

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-use std::env;
 use bdk::Balance;
 use bdk::wallet::{AddressIndex, AddressInfo};
 use bdk::{bitcoin::Network, Wallet, database::MemoryDatabase};
 
 fn main() -> anyhow::Result<()> {
     println!("Hello, wallet!");
-    dotenv::from_filename(".env").unwrap();
-    dotenv::dotenv().ok();
+   
 
-    let descriptor: String = env::var("WALLET_DESCRIPTOR")?;
-
+   // la variable "descriptor" es la misma que hay en el ejemplo en https://github.com/bitcoindevkit/bdk/tree/master/crates/bdk
+    let descriptor: &str = "wpkh(tprv8ZgxMBicQKsPdy6LMhUtFHAgpocR8GC6QmwMSFpZs7h6Eziw3SpThFfczTDh5rW2krkqffa11UpX3XkeTTB2FvzZKWXqPY54Y6Rq4AQ5R8L/84'/0'/0'/0/*)";
     println!("descriptor: {:?}", descriptor);
 
     let wallet: Wallet<MemoryDatabase> = Wallet::new(
-        &descriptor, 
+        descriptor, 
         None,
         Network::Testnet,
         MemoryDatabase::default(),
     )?;
 
+    //Balance de la wallet
     let balance: Balance = wallet.get_balance()?;
 
+    //Adress de la wallet
     let adress: AddressInfo = wallet.get_address(AddressIndex::New)?;
 
     dbg!(balance);
