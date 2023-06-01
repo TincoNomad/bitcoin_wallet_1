9cd7e1da175c4476ce5544c1168714357e62465a
Renzo Tincopa <rtincopa@gmail.com>
2023-05-31 20:27:12 -0500

    aplicando rocket para el API

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,30 @@
+#[macro_use] extern crate rocket;
+
 use bdk::Balance;
 use bdk::wallet::{AddressIndex, AddressInfo};
 use bdk::{bitcoin::Network, Wallet, database::MemoryDatabase};
 
-fn main() -> anyhow::Result<()> {
-    println!("Hello, wallet!");
-   
-
-   // la variable "descriptor" es la misma que hay en el ejemplo en https://github.com/bitcoindevkit/bdk/tree/master/crates/bdk
+#[get("/")]
+fn index() -> String {
+    //Wallet
+    ////la variable "descriptor" es la misma que hay en el ejemplo en https://github.com/bitcoindevkit/bdk/tree/master/crates/bdk
     let descriptor: &str = "wpkh(tprv8ZgxMBicQKsPdy6LMhUtFHAgpocR8GC6QmwMSFpZs7h6Eziw3SpThFfczTDh5rW2krkqffa11UpX3XkeTTB2FvzZKWXqPY54Y6Rq4AQ5R8L/84'/0'/0'/0/*)";
     println!("descriptor: {:?}", descriptor);
 
     let wallet: Wallet<MemoryDatabase> = Wallet::new(
         descriptor, 
-        None,
+         None,
         Network::Testnet,
         MemoryDatabase::default(),
-    )?;
+    ).unwrap();
 
-    //Balance de la wallet
-    let balance: Balance = wallet.get_balance()?;
+    let balance: Balance = wallet.get_balance().unwrap();
+    let address: AddressInfo = wallet.get_address(AddressIndex::New).unwrap();
 
-    //Adress de la wallet
-    let adress: AddressInfo = wallet.get_address(AddressIndex::New)?;
-
-    dbg!(balance);
-    dbg!(adress);
-
-    //print!("balance: {}", balance);
-    //print!("Adres info: {}", adress);
-
-    Ok(())
+    format!("Balance: {:?}\nAddress: {:?}", balance, address)
 }
 
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
